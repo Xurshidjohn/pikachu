ba6ac8c2dd0d3fef4f4505fa1954356584b6bb6a
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-06-20 23:28:29 +0500

    readme.md updates

--- README.md
+++ README.md
@@ -1,13 +1,49 @@
 # PIKACHUUUUUUUU!
 
 ```
+C:\Users\user>pikachu
 PIKACHU TERMINAL
   (\__/)
   (o^.^)
 z_(")(") -> Pika Pika!
 
 Downloading started...
-########################################################################## 100.0%
+#################################################################### 100.0%
 
 SUCCESSFULLY DOWNLOADED!!
+
+C:\Users\user>pikachu
 ```
+
+# Pikachu in Windows Command Line 
+
+```
+C:\Users\xursh>pikachu
+PIKACHU TERMINAL
+  (\__/)
+  (o^.^)
+z_(")(") -> Pika Pika!
+
+Please Enter file url! Pika Pika!
+
+```
+
+# Pikachu in Linux
+
+```
+root@ubuntu:/home/vboxuser# pikachu
+PIKACHU TERMINAL
+  (\__/)
+  (o^.^)
+z_(")(") -> Pika Pika!
+
+Please Enter file url! Pika Pika!
+
+root@ubuntu:/home/vboxuser# 
+```
+
+# PIKACHU IS CURRENTLY ONLY 
+
++ Windows OS
++ Linux OS
++ Mac OS (soon)
--- src/index.c
+++ src/index.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#ifdef _WIN32
+        #include <windows.h>
+
+#endif // _WIN32
+
 int main(int argc, char const *argv[])
 {
     #ifdef _WIN32
-        #include <windows.h>
         if(argv[1]) {
             char *system_command = malloc(sizeof(char) * 120);
         system("cls");
         sprintf(system_command, "curl --progress-bar -O %s", argv[1]);
             printf("\n");
-    printf(" PIKACHU TERMINAL\n");
+    printf("PIKACHU TERMINAL\n");
     printf("  (\\__/)\n");
     printf("  (o^.^)\n");
     printf("z_(\")(\") -> Pika Pika!\n\n");
     printf("Downloading started...\n");
         system(system_command);
         printf("\nSUCCESSFULLY DOWNLOADED!!\n");
-        } else {
-            printf("\nPlease Enter file url! Pika Pika!");
+       } else {
+            printf(" PIKACHU TERMINAL\n");
+            printf("  (\\__/)\n");
+            printf("  (o^.^)\n");
+            printf("z_(\")(\") -> Pika Pika!\n\n");
+            printf("\nPlease Enter file url! Pika Pika!\n\n");
         }
     #elif __linux__
         if(argv[1]) {
@@ -26,7 +35,7 @@ int main(int argc, char const *argv[])
         system("clear");
         sprintf(system_command, "curl --progress-bar -O %s", argv[1]);
             printf("\n");
-    printf(" PIKACHU TERMINAL\n");
+    printf("PIKACHU TERMINAL\n");
     printf("  (\\__/)\n");
     printf("  (o^.^)\n");
     printf("z_(\")(\") -> Pika Pika!\n\n");
