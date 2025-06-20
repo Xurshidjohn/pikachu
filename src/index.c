59e4f9c1af76c63394f37768560a9b7148faa97c
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-06-20 22:01:42 +0500

    pikachoo for linux

--- src/index.c
+++ src/index.c
@@ -17,7 +17,17 @@ int main(int argc, char const *argv[])
         system(system_command);
         printf("\nSUCCESSFULLY DOWNLOADED!!");
     #elif __linux__
-        printf("siz linux tizimidasiz");
+    char *system_command = malloc(sizeof(char) * 120);
+        system("clear");
+        sprintf(system_command, "curl --progress-bar -O %s", argv[1]);
+            printf("\n");
+    printf(" PIKACHU TERMINAL\n");
+    printf("  (\\__/)\n");
+    printf("  (o^.^)\n");
+    printf("z_(\")(\") -> Pika Pika!\n\n");
+    printf("Downloading started...\n");
+        system(system_command);
+        printf("\nSUCCESSFULLY DOWNLOADED!!");
     #else
         printf("Bilmadim");
     #endif
